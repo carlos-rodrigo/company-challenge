38965fcb1e0bdac0ae7b83167e72f717e6e6b303
Carlos Rodrigo <carlos.rodrigo@etermax.com>
2019-09-03 17:53:59 -0300

    url parser first tests

--- url-parser/test/url-parser.test.js
+++ url-parser/test/url-parser.test.js
@@ -1,13 +1,27 @@
 import UrlParser from '../url-parser'
 
 
-test('given an url format string then return a his key', () => {
+test('given an url format and an url to test then return version and 6', () => {
   let urlFormat = "/:version";
   let paramIdentifier = ':'
   let url = "/6";
 
   let expectedReturn = {
                           version: 6
+                       };
+
+  let urlParser = new UrlParser(urlFormat, paramIdentifier);
+
+  expect(urlParser.parse(url)).toEqual(expectedReturn);
+})
+
+test('given an url format and an url to test with 66 as version then return version and 66', () => {
+  let urlFormat = "/:version";
+  let paramIdentifier = ':'
+  let url = "/66";
+
+  let expectedReturn = {
+                          version: 66
                        };
 
   let urlParser = new UrlParser(urlFormat, paramIdentifier);
--- url-parser/url-parser.js
+++ url-parser/url-parser.js
@@ -1,15 +1,42 @@
 export default class UrlParser {
+
     constructor(urlFormat, paramIdentifier){
       this.urlFormat = urlFormat;
-      this.paramIdentifier = paramIdentifier;
+      this.params = this.getParamsNamesFromFormat(urlFormat, paramIdentifier);
+      this.SLASH = '/';
     }
 
     parse(url){
-      let expectedReturn = {
-                          version: 6
-                       };
+      if(this.isUrlMatchWithFormat(url)){
+        var values = this.getValuesFromUrl(url);
 
+        return this.buildMapWithValues(values);
+      }else{
+        return { message: "the url not match with the format" };
+      }
+    }
 
-      return expectedReturn;
+    getParamsNamesFromFormat(url, paramIdentifier){
+      let regex = paramIdentifier + "[a-zA-Z]+";
+      let originalParams = url.match(regex);
+      let finalParams = [];
+      
+      originalParams.forEach(function(element){
+        finalParams.push(element.replace(paramIdentifier, ""));
+      });
+
+      return finalParams;
+    }
+
+    isUrlMatchWithFormat(url){
+      return true;
+    }
+    
+    getValuesFromUrl(url){
+      
+    }
+
+    buildMapWithValues(values){
+
     }
 };
