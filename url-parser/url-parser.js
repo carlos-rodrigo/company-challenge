25651259e1a559e166d214e3e3179f706ee17ee9
Carlos Rodrigo <hi@carlosrodrigo.me>
2019-09-04 00:44:56 -0300

    parsing url params

--- url-parser/url-parser.js
+++ url-parser/url-parser.js
@@ -2,41 +2,65 @@ export default class UrlParser {
 
     constructor(urlFormat, paramIdentifier){
       this.urlFormat = urlFormat;
-      this.params = this.getParamsNamesFromFormat(urlFormat, paramIdentifier);
-      this.SLASH = '/';
+      this.paramIdentifier = paramIdentifier;
     }
 
     parse(url){
-      if(this.isUrlMatchWithFormat(url)){
-        var values = this.getValuesFromUrl(url);
-
-        return this.buildMapWithValues(values);
-      }else{
-        return { message: "the url not match with the format" };
-      }
-    }
-
-    getParamsNamesFromFormat(url, paramIdentifier){
-      let regex = paramIdentifier + "[a-zA-Z]+";
-      let originalParams = url.match(regex);
-      let finalParams = [];
-      
-      originalParams.forEach(function(element){
-        finalParams.push(element.replace(paramIdentifier, ""));
-      });
-
-      return finalParams;
+        if(this.isUrlMatchWithFormat(url)){
+            var values = this.getValuesFromUrl(url);
+            return this.buildMapWithValues(values);
+        }else{
+            return { message: "the url not match with the format" };
+        }
     }
 
     isUrlMatchWithFormat(url){
-      return true;
+        let parts = url.split('/');
+        let formatParts = this.urlFormat.split('/');
+
+        return parts.length == formatParts.length
     }
-    
+
     getValuesFromUrl(url){
-      
+        let urlParts = url.split('/').filter(p => p !== '');
+        let formatParts = this.urlFormat.split('/').filter(p => p !== '');
+
+        let values = [];
+
+        for(let i = 0; i < urlParts; i++){
+            let value = urlParts[i];
+            let param = formatParts[i];
+            if(this.isParam(param))
+                values.push(value);
+        }
+
+        return values;
+    }
+
+    isParam(param){
+        let regex = new RegExp(this.paramIdentifier + "[a-zA-Z]+");
+        return regex.test(param);
     }
 
     buildMapWithValues(values){
+        let map = new Object();
+        let params = this.getParamsNamesFromFormatUrl();
+        for(let i = 0; i < params.length; i++){
+            map[params[i]] = Number(values[i]) != NaN ? Number(values[i]): values[i];
+        }
 
+        return map;
+    }
+
+    getParamsNamesFromFormatUrl(){
+        let regex = this.paramIdentifier + "[a-zA-Z]+";
+        let originalParams = this.urlFormat.match(regex);
+        let finalParams = [];
+
+        originalParams.forEach(function(element){
+            finalParams.push(element.replace(':', ''));
+        });
+
+        return finalParams;
     }
 };
